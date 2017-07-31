51af615c16c3ca0db67394aec84c96107e8712b0
Ueslei de Souza <darkfrontcode@gmail.com>
2017-07-31 14:30:48 -0300

    Base

---
+++ CityAPI/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityAPI.Controllers
+{
+	[Route("api/cities")]
+    public class CitiesController : Controller
+    {
+		[HttpGet()]
+		public IActionResult GetCities()
+		{
+			return Ok(Store.CitiesStore.Current.Cities);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetCity(int id)
+		{
+			var search = Store.CitiesStore.Current.Cities.FirstOrDefault(city => city.Id == id);
+			if(search == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(search);
+		}
+	}
+}
---
+++ CityAPI/Stores/CitiesStore.cs
@@ -0,0 +1,39 @@
+﻿using CityAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityAPI.Store
+{
+    public class CitiesStore
+    {
+		public static CitiesStore Current { get; } = new CitiesStore();
+
+		public List<CityModel> Cities { get; set; }
+
+		public CitiesStore()
+		{
+			Cities = new List<CityModel>(){
+				new CityModel()
+				{
+					Id=1,
+					Name="New York City",
+					Description="The one with that big park."
+				},
+				new CityModel()
+				{
+					Id=2,
+					Name="Antwerp",
+					Description="The one with the cathedral that was never really finished."
+				},
+				new CityModel()
+				{
+					Id=3,
+					Name="Paris",
+					Description="The one with that big tower."
+				}
+			};
+		}
+	}
+}
